5d976e596e78f7073e334f2a5ef6e4f2c9b7f1bb
Sandor Tarcsi <txsanyix@gmail.com>
2020-06-04 16:31:19 +0200

    keepalive, select directory

--- GoproDownloader/__init__.py
+++ GoproDownloader/__init__.py
@@ -1,13 +1,12 @@
-from goprocam import GoProCamera, constants
-import tkinter as tk
-from tkinter import filedialog
-import pygubu
-import json
 import datetime
-import numpy as np
+import json
 import threading
 import time
-import asyncio
+import tkinter as tk
+from tkinter import filedialog
+from tkinter.ttk import Progressbar
+
+from goprocam import GoProCamera
 
 
 class GauPrauHandler:
@@ -18,6 +17,9 @@ class GauPrauHandler:
         self.clsmedialist = list()
         # th = threading.Thread(target=self.keepHopeAlive)
         # th.start()
+        keephopealive = threading.Thread(target=self.goproCamera.KeepAlive, args=())
+        keephopealive.daemon = True
+        keephopealive.start()
         print("hendl")
 
     def listMedia(self):
@@ -27,39 +29,38 @@ class GauPrauHandler:
         # f.write(rawdata)
         # f.close()
         media = (json.loads(rawdata))["media"]
-        thelist = list()
-        self.clsmedialist=thelist
+        self.clsmedialist = list()
         for folder in media:
             foldername = folder["d"]
             print(foldername)
             for ting in folder["fs"]:
                 creationtime = datetime.datetime.fromtimestamp(int(ting["cre"]))
                 # self.invoker.medialist.insert(tk.END, (foldername, time, ting["n"]))
-                thelist.append((foldername, ting["n"], creationtime))
+                self.clsmedialist.append((foldername, ting["n"], creationtime))
         print("listing media")
-        return thelist
+        return self.clsmedialist
 
-    def downMedia(self, selection,selectdir):
+    def downMedia(self, selection, selectdir):
         print(selection, selectdir)
-        # selectarr = [i[1] for i in selection]
         selectarr = [self.clsmedialist[idx] for idx in selection]
         for dikk in selectarr:
             print(dikk[0], dikk[1])
-            self.goproCamera.downloadMedia(dikk[0], dikk[1])
-
+            self.goproCamera.downloadMedia(dikk[0], dikk[1], selectdir + "/" + dikk[1])
 
 
 class GauPrauGUI:
     def __init__(self):
         self.window = tk.Tk()
+        self.window.title("GoPro media downloader")
+        # self.progress = Progressbar(self.window, orient=tk.HORIZONTAL, length=100, mode='indeterminate')
         self.handler = GauPrauHandler(self)
-        listbutton = tk.Button(text="nyomjad", command=self.fillListbocks)
-        downbutton = tk.Button(text="dánlód", command=self.downloadselected)
-        listbutton.pack()
-        downbutton.pack()
+        self.listbutton = tk.Button(text="List media", command=self.fillListbocks)
+        self.downbutton = tk.Button(text="Download selected", command=self.downloadselected)
+        self.listbutton.pack()
+        self.downbutton.pack()
         self.medialist = tk.Listbox(selectmode=tk.MULTIPLE, width=60)
         self.medialist.pack()
-        clearbutton = tk.Button(text="kijelöléstörlés", command=self.clearselection)
+        clearbutton = tk.Button(text="Clear selection", command=self.clearselection)
         clearbutton.pack()
         # self.window.protocol("WM_DELETE_WINDOW", self.kloze())
         self.window.mainloop()
@@ -67,23 +68,14 @@ class GauPrauGUI:
     def fillListbocks(self):
         dalist = self.handler.listMedia()
         self.medialist.insert(tk.END, *dalist)
-        
+
     def clearselection(self):
         self.medialist.selection_clear(0, tk.END)
 
     def downloadselected(self):
-        selected= self.medialist.curselection()
-        selectdir= filedialog.askdirectory(title="jelöljed ki hova")
+        selected = self.medialist.curselection()
+        selectdir = filedialog.askdirectory(title="Select download folder")
         self.handler.downMedia(selected, selectdir)
 
-
-
-# goproCamera = GoProCamera.GoPro()
-# mediaz= goproCamera.listMedia(False,True)
-
-# for media in mediaz:
-# print (media)
-# print(mediaz)
-# goproCamera.downloadMedia("100GOPRO","GH014107.MP4");
 
 gui = GauPrauGUI()
